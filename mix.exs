655a037c1325c648e8d440cf7c3dd206ce3a3265
AkitaOnRails <fabioakita@gmail.com>
2015-12-17 12:49:39 -0200

    Upgrading to Phoenix 1.1

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule ExPusherLite.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
-     {:phoenix_ecto, "~> 1.1"},
+    [{:phoenix, "~> 1.1"},
+     {:phoenix_ecto, "~> 2.0"},
+     {:phoenix_html, "~> 2.3"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
      {:joken, "~> 1.0.0"},
--- web/views/changeset_view.ex
+++ web/views/changeset_view.ex
@@ -1,9 +1,24 @@
 defmodule ExPusherLite.ChangesetView do
   use ExPusherLite.Web, :view
 
+  def translate_error({msg, opts}) do
+    String.replace(msg, "%{count}", to_string(opts[:count]))
+  end
+  def translate_error(msg), do: msg
+
+  @doc """
+  Traverses and translates changeset errors.
+
+  See `Ecto.Changeset.traverse_errors/2` and
+  `MyApp.ErrorHelpers.translate_error/1` for more details.
+  """
+  def translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  end
+
   def render("error.json", %{changeset: changeset}) do
     # When encoded, the changeset returns its errors
     # as a JSON object. So we just pass it forward.
-    %{errors: changeset}
+    %{errors: translate_errors(changeset)}
   end
 end
