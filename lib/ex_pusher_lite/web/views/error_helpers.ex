fe50a4d1358f7976e66f26f95b5039cc79a62b21
Fabio Akita <fabioakita@gmail.com>
2017-07-03 17:30:02 -0300

    another missing Web namespace missing in the upgrade

--- lib/ex_pusher_lite/web/views/error_helpers.ex
+++ lib/ex_pusher_lite/web/views/error_helpers.ex
@@ -32,9 +32,9 @@ defmodule ExPusherLite.Web.ErrorHelpers do
     #     dgettext "errors", "is invalid"
     #
     if count = opts[:count] do
-      Gettext.dngettext(ExPusherLite.Gettext, "errors", msg, msg, count, opts)
+      Gettext.dngettext(ExPusherLite.Web.Gettext, "errors", msg, msg, count, opts)
     else
-      Gettext.dgettext(ExPusherLite.Gettext, "errors", msg, opts)
+      Gettext.dgettext(ExPusherLite.Web.Gettext, "errors", msg, opts)
     end
   end
 end
